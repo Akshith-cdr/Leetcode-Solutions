2575511461cc36b63db7589058d8fd14ab6686d5
VEERISETTY AKSHITH <akshith.v24@iiits.in>
2026-08-17 18:41:49 +0530

    Time: 3 ms (23.88%), Space: 19.3 MB (58.47%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        digit,max1,max2=0,0,0
+
+        while n:
+            digit=n%10
+            
+            if digit>max1:
+                max2=max1
+                max1=digit
+            elif digit>max2:
+                max2=digit
+            
+            n//=10
+        
+        return max1*max2
