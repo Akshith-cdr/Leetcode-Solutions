f54caad4b15efece3d14a6e710e48f1ef3426409
VEERISETTY AKSHITH <akshith.v24@iiits.in>
2026-08-18 16:51:42 +0530

    Time: 15 ms (57.98%), Space: 19.4 MB (34.31%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.py
+++ 1006-clumsy-factorial/1006-clumsy-factorial.py
@@ -1,20 +1,31 @@
 class Solution:
     def clumsy(self, n: int) -> int:
-        stack=[]
-        op=0
-        
-        stack.append(n)
-        
-        for i in range(n-1,0,-1):
+        total,num,op=0,n,0
+        flag=True
+        n-=1
+
+        while n>0:
             if op==0:
-                stack[-1]*=i
+                num*=n
             elif op==1:
-                stack[-1]=int(stack[-1]/i)
+                num//=n
             elif op==2:
-                stack.append(i)
-            else:
-                stack.append(-i)
-            
+                if flag:
+                    total+=num
+                    flag=False
+                else:
+                    total-=num
+                total+=n
+                num=0
+            elif op==3:
+                num=n
+
             op=(op+1)%4
-        
-        return sum(stack)
+            n-=1
+
+        if flag:
+            total+=num
+        else:
+            total-=num
+
+        return total
